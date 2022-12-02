934923560e30ffcad5b57e7ed771674cb4a27e52
Travankulusz <zsombor018@gmail.com>
2022-12-02 11:09:00 +0100

    DesignValtozasok
    HomeFragmant átalakítás+háttér
    
    SummonerInfo Fragmant átalakítás+háttér

--- app/src/main/java/hu/leagueoflegends/android_api_app_beadando/summinfo/InterfaceForSumm.java
+++ app/src/main/java/hu/leagueoflegends/android_api_app_beadando/summinfo/InterfaceForSumm.java
@@ -11,6 +11,10 @@ public interface InterfaceForSumm {
 //https://eun1.api.riotgames.com/lol/summoner/v4/
 //        summoners/by-name/AsrielHUN?api_key=RGAPI-93891ff8-dbea-4061-8932-d0a5fb1b51a8
 
+<<<<<<< Updated upstream
     @GET("{summname}?api_key=RGAPI-6376bc02-0d61-4f76-8162-bd1a6ac9e82c")
+=======
+    @GET("{summname}?api_key=RGAPI-b28c0fd5-0d9b-4d78-b658-fbff6d2df73f")
+>>>>>>> Stashed changes
     Call<ModelForSumm> getDataForSumm(@Path("summname") String SummName);
 }
